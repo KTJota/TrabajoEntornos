5e84ddb513f7dd54f132f94cb7a93b2985842134
KTJota <jesuusxx@gmail.com>
2022-03-17 08:27:51 +0100

    Clase abstracta Producto, version 1.0
    con constructor y métodos getters básicos

---
+++ TrabajoEntornos/src/Producto.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jmuela
+ */
+public abstract class Producto {
+    private String nombre;
+    private double precio;
+    private int cantidad;
+    
+    public Producto(String n, double p, int c){
+        nombre = n;
+        precio = p;
+        cantidad = c;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+    
+}
