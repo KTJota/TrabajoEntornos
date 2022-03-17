2e8b71d44f5e1453605bf780556af881df74d94f
L3nd4r <thunderray16@gmail.com>
2022-03-17 18:02:48 +0100

    Clase Libro

---
+++ AplicacionGestionComercio/src/Libro.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.entornos;
+
+public class Libro implements Producto{
+    private String autor;
+    private int paginas;
+    private String tipo;
+    
+    public Libro(String n, double p, int c, String aut, int pag, String tip){
+        super(n,p,c);
+        autor=aut;
+        paginas=pag;
+        tipo=tip;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
